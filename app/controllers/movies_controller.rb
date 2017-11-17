720df7808e7e386ad893fc2d1aabddb271d6cbbb
Luciano Oliveira <luciano101@msn.com>
2017-11-16 19:44:39 -0800

    created find_movie partial, and detail page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,15 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
+
   def index
     @movies = Movie.all.order('created_at DESC')
   end
 
   def new
     @movie = Movie.new
+  end
+
+  def show
   end
 
   def create
@@ -19,6 +24,10 @@ class MoviesController < ApplicationController
 
   private
   def movie_params
-    params.require(:movie).permit(:title, :description, :diretor)
+    params.require(:movie).permit(:title, :description, :director)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:id])
   end
 end
