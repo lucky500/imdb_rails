1cc1dc55207be3b6fb7059176af16928f64d1ec5
Luciano Oliveira <luciano101@msn.com>
2017-11-25 20:12:11 -0800

    add jquery-raty for stars review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,51 @@
+class ReviewsController < ApplicationController
+  before_action :find_movie
+  before_action :find_review, only: [:edit, :update, :destroy]
+  
+  def new
+    @review = Review.new
+  end
+
+  def create
+    
+    @review = Review.new(review_params)
+    @review.movie_id = @movie.id
+    @review.user_id = current_user.id
+
+    if @review.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to movie_path(@movie)
+  end
+
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
   root 'movies#index'
 end
